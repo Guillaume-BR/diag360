f6b3c0e60f444b2f2e865d7ed5a8495c411ef0b9
Ange Mat <angeliqueetmarie@hotmail.fr>
2026-01-10 16:54:54 +0100

    Ajout de l'epci carto_sante.py

--- src/carto_sante.py
+++ src/carto_sante.py
@@ -5,6 +5,7 @@ import pandas as pd
 import os
 import sys
 import re 
+import duckdb
 
 """Ce script télécharge les fichiers Excel depuis les URLs fournies,
 l'objectif est de récupérer les fichiers Excel depuis carto_sante.
@@ -12,19 +13,16 @@ l'objectif est de récupérer les fichiers Excel depuis carto_sante.
 Contrainte : 
 - Il faut que les liens des téléchargements soient toujours les mêmes
 - Il faut que le format des fichiers Excel soit toujours le même (en-tête à la 9e ligne)
+- Il faut que la colonne "Code commune INSEE" soit toujours présente dans les fichiers Excel
 """
-base_dir = Path(__file__).resolve().parent.parent  # racine du projet diag360
-data_dir = base_dir / "data" / "data_cat_nat"
+base_dir = os.path.dirname(os.path.dirname(__file__))  # racine du projet diag360
 
-raw_dir = data_dir / "raw"
-processed_dir = data_dir / "processed"
+raw_dir = os.path.join(base_dir, "data", "data_cat_nat", "raw")
 
-raw_dir.mkdir(parents=True, exist_ok=True)
-processed_dir.mkdir(parents=True, exist_ok=True)
-
+os.makedirs(raw_dir, exist_ok=True)
 # 1. Ajouter le dossier parent de src (le projet) au path pour importer utils
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-from utils.functions import download_file, create_dataframe_communes
+from utils.functions import download_file, float_to_codepostal
 
 # 2. On récupère le dossier "diag360" (parent de "src")
 root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -63,10 +61,48 @@ for file in chemin_complet_urls:
         # 5. On remplit le dictionnaire final : { nom_fichier : nom_onglet }
     dict_fichier_onglet[file] = nom_onglet_recent[0]
 
+df_apl_medecins = pd.read_excel(
+    chemin_complet_urls[0],
+    sheet_name=dict_fichier_onglet[chemin_complet_urls[0]],
+    header=8,
+)
+df_apl_infirmiers = pd.read_excel(
+    chemin_complet_urls[1],
+    sheet_name=dict_fichier_onglet[chemin_complet_urls[1]],
+    header=8,
+)
+df_apl_chirurgien_dentiste = pd.read_excel(
+    chemin_complet_urls[2],
+    sheet_name=dict_fichier_onglet[chemin_complet_urls[2]],
+    header=8,
+)
+df_apl_sage_femme = pd.read_excel(
+    chemin_complet_urls[3],
+    sheet_name=dict_fichier_onglet[chemin_complet_urls[3]],
+    header=8,
+)
+
+
 # On récupère la liste des communes par EPCI pour jointure
-df_com = create_dataframe_communes(raw_dir)
+com_url = (
+        "https://www.data.gouv.fr/api/1/datasets/r/f5df602b-3800-44d7-b2df-fa40a0350325"
+    )
+download_file(com_url, extract_to=raw_dir, filename="communes_france_2025.csv")
+df_com = pd.read_csv(os.path.join(raw_dir, "communes_france_2025.csv"))
+df_com = float_to_codepostal(df_com, "code_postal")
+
+def commune_to_epci(table_to_join, table_ref):
+    query = f"""
+    SELECT  dam.*, dc.epci_code AS siren, dc.epci_nom AS epci_nom
+    FROM {table_to_join} as dam
+    JOIN {table_ref} as dc
+        on dam."Code commune INSEE" = dc.code_insee
+    """
+    return duckdb.sql(query).df()
 
 
+medecin_epci = commune_to_epci("df_apl_medecins","df_com")
+infirmier_epci = commune_to_epci("df_apl_infirmiers","df_com")
+chirurgien_dentiste_epci = commune_to_epci("df_apl_chirurgien_dentiste","df_com")
+sage_femme_epci = commune_to_epci("df_apl_sage_femme","df_com")
 
-# On charge les données de l'onglet récent
-#df_apl_medecins = pd.read_excel(chemin_complet, sheet_name=onglet_recent, header=8)
